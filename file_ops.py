99e575c6d3f6fd6ee42ba8599261603b37a02a5a
JosiePark <j.park15@ic.ac.uk>
2018-06-27 16:29:29 +0100

    further organisation

--- file_ops.py
+++ file_ops.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 def make_data_object_name(
-        dataset_name,
+		dataset_name,
         year, month, day, hour,
         realization, forecast_period):
     """Create a string formatted to give a filename in the MOGREPS dataset."""
@@ -10,14 +10,16 @@ def make_data_object_name(
         dataset_name, year, month, day, hour, realization, forecast_period)
 
 def write_mean_netCDF(year,month,no_days,temp_mean, pres_mean, humidity_mean, wind_mean):
+
+    import netCDF4 as nc
+    import numpy as np
     
-    template_string ="ensemble_means_{:02d}{:02d}.nc"
+    template_string ="ensemble_mean_data/ensemble_means_{:02d}{:02d}.nc"
     
     file_name = template_string.format(
         year, month)
     
     dataset = nc.Dataset(file_name,'w')
-    
     
     dataset.createDimension('Days',no_days)
     
@@ -26,11 +28,103 @@ def write_mean_netCDF(year,month,no_days,temp_mean, pres_mean, humidity_mean, wi
     humidity_var = dataset.createVariable('Humidity',np.float32,('Days'))
     wind_var = dataset.createVariable('Wind Speed',np.float32,('Days'))
     
-    temp_var[:no_days] = temp_mean
-    pres_var[:no_days] = pres_mean
-    humidity_var[:no_days] = humidity_mean
-    wind_var[:no_days] = wind_mean
+    temp_var[:] = temp_mean
+    pres_var[:] = pres_mean
+    humidity_var[:] = humidity_mean
+    wind_var[:] = wind_mean
     
     dataset.close()
     
+    return
+
+def read_test_data(year,month,day,hour,test_size):
+    
+    import netCDF4 as nc
+
+    temp_test = []
+    pressure_test = []
+    humidity_test = []
+    u_wind_test = []
+    v_wind_test = []
+
+    for i in range(test_size):
+
+        mogreps_data = make_data_object_name('mogreps-uk',year,month,day,hour,12-i,3)
+        print(mogreps_data)
+        dataset = nc.Dataset(mogreps_data,'r')
+
+        # read temperature
+        
+        temp = dataset.variables['air_temperature'][0,0,lat,lon]
+        
+        # read pressure
+        
+        pressure = dataset.variables['surface_air_pressure'][0,lat,lon]
+        
+        # read humidity
+        
+        humidity = dataset.variables['relative_humidity'][0,0,lat,lon]
+        
+        # read wind_speed
+        
+        u_wind = dataset.variables['x_wind'][0,0,lat,lon]
+        
+        v_wind = dataset.variables['y_wind'][0,0,lat,lon]
+        
+      	wind_speed = np.sqrt(u_wind**2 + v_wind**2)
+	
+		# append test data
+
+        temp_test.append(temp)
+        pressure_test.append(pressure)
+        humidity_test.append(humidity)
+        wind_test.append(wind_speed)
+
+        
+        # close file
+        
+        dataset.close()
+
+    return temp_test,pres_test,humidity_test,wind_test
+
+def write_test_data(year,month,n_days,hour,test_size):
+    
+    import netCDF4 as nc
+    import numpy as np
+
+    for d in range(n_days):
+
+		# read test data
+
+        temp_test,pressure_test,humidity_test,wind_test = read_test_data(year,month,d,hour,test_size)
+
+	# name file
+
+    template_string ="test_data/test_data_{:02d}{:02d}.nc"
+    
+    file_name = template_string.format(
+        year, month)
+    dataset = nc.Dataset(file_name,'w')
+
+	# create dimensions
+
+    dataset.createDimension('Day',n_days)
+
+ 	# create variables
+
+    temp_var = dataset.createVariable('Temperature',np.float32,('Days'))
+    pres_var = dataset.createVariable('Pressure',np.float32,('Days'))
+    humidity_var = dataset.createVariable('Humidity',np.float32,('Days'))
+    wind_var = dataset.createVariable('Wind Speed',np.float32,('Days'))
+
+	# write to variables
+
+    temp_var[:] = temp_test
+    pres_var[:] = pressure_test
+    humidity_var[:] = humidity_test
+    wind_var[:] = wind_test
+
+    dataset.close()
+
+
     return
--- write_mean_data.py
+++ write_mean_data.py
@@ -6,24 +6,41 @@ import netCDF4 as nc
 import ensemble_stats
 import file_ops
 
+# set variables
+
 ensemble_size = 8
-n_months = 1
-n_days = 31
+month = 2
+year = 2014
+hour = 3
+
+# determine correct number of days
+if month in [1,3,5,7,8,10,12]:
+	n_days = 31
+elif month in [4,6,9,11]:
+	n_days =30
+else:
+	n_days = 28
 
 temp_mean = []
 pres_mean = []
 humidity_mean = []
 wind_mean = []
 
-for m in range(n_months):
-    for d in range(n_days):
-        temp,pres,humidity,wind = ensemble_stats.calculate_mean(0,0,2014,m+1,d+1,3,ensemble_size)
+#for d in range(n_days):
+#	temp,pres,humidity,wind = ensemble_stats.calculate_mean(0,0,2014,month,d+1,3,ensemble_size)
         
-        temp_mean.append(temp)
-        pres_mean.append(pres)
-        humidity_mean.append(humidity)
-        wind_mean.append(wind)
+#        temp_mean.append(temp)
+#        pres_mean.append(pres)
+#        humidity_mean.append(humidity)
+#        wind_mean.append(wind)
+
 
 # write means to file
 
-write_mean_netCDF(2014,1,31,temp_mean,pres_mean,humidity_mean,wind_mean)
+#file_ops.write_mean_netCDF(year,month,n_days,temp_mean,pres_mean,humidity_mean,wind_mean)
+
+# write test_data to file
+
+test_size = 12 - ensemble_size
+
+file_ops.write_test_data(year,month,n_days,hour,test_size)
